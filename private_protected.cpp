6c9f16adec99b1c38c1054d73294c7c3f038f71d
RITESH PRASHANT UPADHYE <154613906+RITESH0706@users.noreply.github.com>
2024-05-06 14:44:34 +0530

    Add files via upload

---
+++ private_protected.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>
+using namespace std;
+
+class Person {
+	int id;
+	char name[100];
+
+public:
+	void set_p()
+	{
+		cout << "Enter the Id:";
+		cin >> id;
+		cout << "Enter the Name:";
+		cin >> name;
+	}
+
+	void display_p()
+	{
+		cout << endl <<"Id: "<< id << "\nName: " << name <<endl;
+	}
+};
+
+class Student : protected Person {
+	char course[50];
+	int fee;
+
+public:
+	void set_s()
+	{
+		set_p();
+		cout << "Enter the Course Name:";
+		cin >> course;
+		cout << "Enter the Course Fee:";
+		cin >> fee;
+	}
+
+	void display_person()
+	{
+		display_p();
+		cout <<"Course: "<< course << "\nFee: " << fee << endl;
+	}
+};
+
+int main()
+{
+	Student s;
+	s.set_s();
+	s.display_person();
+	return 0;
+}
+
